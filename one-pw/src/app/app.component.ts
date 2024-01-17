10d46056f3bbf489668e17c6f7550c803507c97d
rihammisuk <riham659misuk@gmail.com>
2024-01-17 23:12:48 +0600

    app page chaned

--- one-pw/src/app/app.component.ts
+++ one-pw/src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'one-pw-The-Modern-Angular-Bootcamp';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeLetters = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    this.password = 'My Password!!!';
+  }
 }
