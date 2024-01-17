d26dbb338005e52517ee22d86372550baf1e13f3
rihammisuk <riham659misuk@gmail.com>
2024-01-17 22:59:25 +0600

    =Initial

---
+++ The-Modern-Angular-Bootcamp/one-pw-The-Modern-Angular-Bootcamp/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'one-pw-The-Modern-Angular-Bootcamp'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('one-pw-The-Modern-Angular-Bootcamp');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('one-pw-The-Modern-Angular-Bootcamp app is running!');
+  });
+});
