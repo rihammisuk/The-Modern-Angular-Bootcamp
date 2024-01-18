2c1552e5dbce38d776deaac9606f7d91bfaf79b6
rihammisuk <riham659misuk@gmail.com>
2024-01-18 12:14:19 +0600

    02 - Understanding Angular Template Syntax password generator

--- one-pw/src/app/app.component.ts
+++ one-pw/src/app/app.component.ts
@@ -10,19 +10,46 @@ export class AppComponent {
   includeNumbers = false;
   includeSymbols = false;
   password = '';
+  length = 0;
 
   onChangeUseLetters() {
     this.includeLetters = !this.includeLetters;
   }
 
   onChangeUseNumbers() {
-    this.includeLetters = !this.includeNumbers;
+    this.includeNumbers = !this.includeNumbers;
   }
   onChangeUseSymbols() {
     this.includeSymbols = !this.includeSymbols;
   }
 
   onButtonClick() {
-    this.password = 'My Password!!!';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onChangeLength(event: any) {
+    const parsedValue = parseInt(event.target.value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
   }
 }
