0f4e4744932587200dfa16a59128e2877c9a2a61
rihammisuk <riham659misuk@gmail.com>
2024-01-18 18:18:23 +0600

    05 -Mastering Pipes Pipes

--- three-pipes/src/app/app.component.ts
+++ three-pipes/src/app/app.component.ts
@@ -3,8 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'three-pipes';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height: number = 0;
+  miles: number = 0;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000,
+  };
+
+  onMilesChange(event: any) {
+    let value = event.target.value;
+    this.miles = parseFloat(value);
+  }
+
+  onHeightChange(event: any) {
+    let value = event.target.value;
+    this.height = parseFloat(value);
+  }
+
+  onNameChange(event: any) {
+    let value = event.target.value;
+    this.name = value;
+  }
+
+  onDateChange(event: any) {
+    let value = event.target.value;
+    this.date = value;
+  }
+
+  onAmountChange(event: any) {
+    let value = event.target.value;
+    this.amount = value;
+  }
 }
