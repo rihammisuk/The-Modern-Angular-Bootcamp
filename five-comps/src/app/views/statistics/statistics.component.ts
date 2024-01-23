996ee9f02330b190d39eb781a5b83edf5a8fb8db
rihammisuk <riham659misuk@gmail.com>
2024-01-23 14:21:36 +0600

    10 - Advanced Component Routing

---
+++ five-comps/src/app/views/statistics/statistics.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics',
+  templateUrl: './statistics.component.html',
+  styleUrls: ['./statistics.component.css'],
+})
+export class StatisticsComponent implements OnInit {
+  @Input() data = [];
+
+  ngOnInit(): void {}
+}
