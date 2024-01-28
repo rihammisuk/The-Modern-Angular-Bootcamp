aa150bf18b571a8fa2df032184219a44e0291ad2
rihammisuk <riham659misuk@gmail.com>
2024-01-28 12:18:09 +0600

    INITIAL COMMIT

---
+++ creditcard/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CardFormComponent } from './card-form/card-form.component';
+
+@NgModule({
+  declarations: [AppComponent, CardFormComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
